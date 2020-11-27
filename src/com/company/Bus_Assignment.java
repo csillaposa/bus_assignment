b30fd131788396eca783020c758194561eccdf87
Csilla <posa.csilla@gmail.com>
2020-11-27 16:07:49 +0100

    Connect to bus_assignment
    Connected to the right database

--- src/com/company/Bus_Assignment.java
+++ src/com/company/Bus_Assignment.java
@@ -6,11 +6,13 @@ import java.sql.SQLException;
 
 public class Bus_Assignment {
     public static void main(String[] args) throws SQLException {
-        String url = "JDBC:mysql://localhost:3306/demo_afp?serverTimezone=UTC";
+        String url = "JDBC:mysql://localhost:3306/bus_assignment?serverTimezone=UTC";
         String user = "trip_admin";
         String password = "tripadmin";
 
         // connect to database
         Connection bus_assignment_db = DriverManager.getConnection(url, user, password);
     }
+
+    // create methods to add busses
 }
