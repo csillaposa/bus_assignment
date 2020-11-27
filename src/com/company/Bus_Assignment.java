b15acbb29c98de5c9427cacb3021285c1c066867
Csilla <posa.csilla@gmail.com>
2020-11-27 16:01:52 +0100

    Created connection with database
    Created the connection between IntelliJ and MySQL database using the MySQL - Java connector, set up user and connection

---
+++ src/com/company/Bus_Assignment.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Bus_Assignment {
+    public static void main(String[] args) throws SQLException {
+        String url = "JDBC:mysql://localhost:3306/demo_afp?serverTimezone=UTC";
+        String user = "trip_admin";
+        String password = "tripadmin";
+
+        // connect to database
+        Connection bus_assignment_db = DriverManager.getConnection(url, user, password);
+    }
+}
